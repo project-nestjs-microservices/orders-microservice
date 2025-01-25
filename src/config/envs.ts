7c13194c04c7e6aa71910da058cff557ee589472
Jhossuan <jhossuacamposd86@gmail.com>
2025-01-24 21:24:55 -0500

    product microservice added in order microservice

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,16 @@ import * as joi from 'joi'
 
 interface EnvVars {
   PORT: number;
+
+  PRODUCTS_MICROSERVICE_HOST: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
+
+  PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+  PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
 }).unknown(true)
 
 const { error, value } = envsSchema.validate(process.env);
@@ -19,4 +25,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+
+  productsMicroserviceHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  productsMicroservicePort: envVars.PRODUCTS_MICROSERVICE_PORT,
 }
